e5c232cca1cdbdfd6c328d957f15d4a6b5a6887d
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-05-19 16:10:22 +0530

    using useEffect show a date and time (vid-52)

--- react-thapa/src/Components/hooks/UseEffect/index.jsx
+++ react-thapa/src/Components/hooks/UseEffect/index.jsx
@@ -2,19 +2,37 @@ import { useEffect, useState } from "react";
 
 export const ReactUseEffect=()=>{
 
-    const[count,setCount]=useState(0);
+    // const[count,setCount]=useState(0);
+
+    // useEffect(()=>{
+    //     console.log("counterr : ",count)
+    // },[count]);
+
+    // return(
+    //     <>
+    //         <div>
+    //             <h1>UseEffectHook</h1>
+    //             <p>count : {count}</p>
+    //             <button onClick={()=>setCount(count+1)}>Increment</button>
+    //         </div>
+    //     </>
+    // );
+
+
+    const[date,setDate]=useState(0);
 
     useEffect(()=>{
-        console.log("counterr : ",count)
-    },[count]);
+        const updatedate=new Date();
+        setInterval(()=>{
+            setDate(updatedate.toLocaleTimeString());
+            
+        },1000)
+        console.log(updatedate);
+    },[date])
 
     return(
         <>
-            <div>
-                <h1>UseEffectHook</h1>
-                <p>count : {count}</p>
-                <button onClick={()=>setCount(count+1)}>Increment</button>
-            </div>
+            <h1>Date : {date}</h1>
         </>
-    );
+    )
 };
