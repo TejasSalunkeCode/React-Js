22d535cbdc473ba75ba2ff67027127cacadd4e93
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-05-21 14:45:02 +0530

    pokemaon card using asyn and await [vid-58]

--- react-thapa/src/Components/hooks/UseEffect/HowNotToFectchApi.jsx
+++ react-thapa/src/Components/hooks/UseEffect/HowNotToFectchApi.jsx
@@ -3,12 +3,37 @@ import { useEffect, useState } from "react";
 
 export const HowNotToFetchApli =()=>{
     const[api,setApi]=useState([]);
+    const[loading,setLoading]=useState(true);
+    const[error,setError]=useState(null);
     
-    const fetchpokemonn=()=>{
-        fetch("https://pokeapi.co/api/v2/pokemon/pikachu").then((res)=>res.json())
-        .then((data)=>setApi(data))
-        .catch((error)=>console.log(error));
+    const  ApiOriginal="https://pokeapi.co/api/v2/pokemon/pikachu";
+    // const fetchpokemonn=()=>{
+    //     fetch("https://pokeapi.co/api/v2/pokemon/pikachu").then((res)=>res.json())
+    //     .then((data)=>{
+    //         setApi(data);
+    //         setLoading(false);
+    // })
+    //     .catch((error)=>{
+    //         console.log(error)
+    //         setLoading(false)
+    //         setError(error)
+    //     });
+    // }
+
+    const fetchpokemonn=async()=>{
+        try{
+            const res=await fetch(ApiOriginal);
+            const data=await res.json();
+            setApi(data);
+            setLoading(false);
+        }catch(error){
+            console.log(error);
+            setError(error);
+            setLoading(false);
+        }
     }
+    
+
 
     useEffect(()=>{
         fetchpokemonn();
@@ -16,7 +41,26 @@ export const HowNotToFetchApli =()=>{
 
      console.log(api)
 
-     if(api){
+     if(loading){
+        return(
+            <>
+                <div>
+                    <h1>Loading..</h1>
+                </div>
+            </>
+        )
+     }
+     if(error){
+        return(
+            <>
+                <div>
+                    <h1>{error.message}</h1>
+                </div>
+            </>
+        )
+     }
+
+
     return(
         <>
 
@@ -31,10 +75,20 @@ export const HowNotToFetchApli =()=>{
                         className="pokemon-image" />
                     </figure>
                     <h1>{api.name}</h1>
+                    <div className="grid-three-cols">
+                        <p className="pokemon-info">
+                            Height : <span>{api.height}</span>
+                        </p>
+                             <p className="pokemon-info">
+                            Weight : <span>{api.weight}</span>
+                        </p>
+                             <p className="pokemon-info">
+                            speed : <span>{api.stats[5].base_stat}</span>
+                        </p>
+                    </div>
                 </li>
             </ul>
             </div>
         </>
     )
-}
 }
