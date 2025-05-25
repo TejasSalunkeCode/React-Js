35a3de85cd50caba8d9a1886ffe8b87902760e5a
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-05-25 16:41:43 +0530

    old way and new way of react router impementation

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -1,31 +1,45 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider,Route } from "react-router";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Movie } from "./pages/Movie";
 import { Contact } from "./pages/Contact";
+// import { Route } from "react-router-dom";
 
  const APP=()=>{
 
-  const router=createBrowserRouter([
-    {
-      path:"/",
-      element:<Home/>
-    },
-    {
-      path:"/about",
-      element:<About/>
+  // const router=createBrowserRouter([
+  //   {
+  //     path:"/",
+  //     element:<Home/>
+  //   },
+  //   {
+  //     path:"/about",
+  //     element:<About/>
 
-    },
+  //   },
 
-    {
-      path:"/movie",
-      element:<Movie/>
-    },
-    {
-      path:"/contact",
-      element:<Contact/>
-    },
-  ]);
+  //   {
+  //     path:"/movie",
+  //     element:<Movie/>
+  //   },
+  //   {
+  //     path:"/contact",
+  //     element:<Contact/>
+  //   },
+  // ]);
+
+
+  //traditional meathod
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/movie" element={<Movie/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Route>
+    )
+  );
 
   return <RouterProvider router={router}/>; 
 };
