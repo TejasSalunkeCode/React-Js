b8640e538af65c4793ec57e5a81965bba2bc4da1
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-05-26 14:33:26 +0530

    useNavigate [vid-81]

--- react_router/src/pages/ErrorPage.jsx
+++ react_router/src/pages/ErrorPage.jsx
@@ -1,7 +1,13 @@
-import { useRouteError } from "react-router-dom";
-import { NavLink } from "react-router-dom";
+import { useRouteError,useNavigate } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 export const ErrorPage=()=>{
     const error=useRouteError();
+    const navigate=useNavigate();
+    console.log(navigate);
+    
+    const handleGoBack=()=>{
+        navigate(-1);
+    }
     if(error.status===404){
         return(
             <section className="error-section">
@@ -10,15 +16,17 @@ export const ErrorPage=()=>{
     <img src="/react_router/src/assets/errorIMg.png" alt="" />
                 </figure>
                 <div className="text-center">
-                    <p className="p-a">
+                    <p className="p-a" >
                         The Page you were looking for could not be found
                     </p>
-                    <p className="p-b">...Back to Previous Page</p>
+                    <p className="p-b" >...Back to Previous Page</p>
                 </div>
             </div>
-            <NavLink to="/" className="btn">
+            {/* <NavLink to="/" className="btn">
                 Go Back to Home Page
-            </NavLink>
+            </NavLink> */}
+            <button className="btn" onClick={handleGoBack}>Go Back</button>
+
             </section>
         )
     };
